7175054d9b8cccf73de2913399fceec7058c90b9
Thaung Pyae Phyo <thaungpyaephyo@myansis.com>
2023-11-20 23:01:01 +0630

    initial comment

---
+++ resources/views/components/admin/nav.blade.php
@@ -0,0 +1,25 @@
+<div class="bg-zinc-800">
+    <div class="flex justify-between mx-10 py-4">
+        <div>
+            <h1 class="text-white text-2xl font-bold font-mono">Admin Demo</h1>
+        </div>
+        <div class="">
+            <div class="flex items-center justify-center gap-6">
+                <div class="relative">
+                    <input type="text" class="pl-8 text-white rounded-lg bg-zinc-700 focus:outline-none"
+                        placeholder="search">
+                        <span class="absolute left-2 top-2 text-zinc-500"><i class="fa-solid fa-magnifying-glass"></i></span>
+                </div>
+
+                <div class="relative">
+                    <span class="text-white"><i class="far fa-bell fa-xl"></i></span>
+                    <span
+                        class="absolute right-0 left-4 -top-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">3</span>
+                </div>
+                <div>
+                    <span class="rounded-full bg-gray-900 text-white p-3">DU</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
